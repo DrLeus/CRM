9197c9b2da70b14308426ca0c497495fea1e0b24
andrey.leus80@gmail.com <andrey.leus80@gmail.com>
2016-04-06 21:45:47 +0300

    dev

--- main/com/ua/smarterama/andrey/leus/CRM/Main.java
+++ main/com/ua/smarterama/andrey/leus/CRM/Main.java
@@ -4,4 +4,7 @@ package com.ua.smarterama.andrey.leus.CRM;
  * Created by Admin on 06.04.2016.
  */
 public class Main {
+    public static void main(String[] args) {
+        
+    }
 }
--- test/com/ua/smarterama/andrey/leus/CRM/Test.java
+++ test/com/ua/smarterama/andrey/leus/CRM/Test.java
@@ -1,7 +1,5 @@
 package com.ua.smarterama.andrey.leus.CRM;
 
-/**
- * Created by Admin on 06.04.2016.
- */
+
 public class Test {
 }
